91e2ad7921423b936baec66c9d1037d09d397c43
chandram13 <mschandr@uci.edu>
2022-08-15 14:50:20 -0700

    Completed bladder function given particular characteristics.

--- main.cpp
+++ main.cpp
@@ -5,27 +5,29 @@ class formsofCancer() {
     std::cout << "In this program, the user will understand, diagnose, and prevent multiple forms of cancer." << std::endl;
 
     public static void Bladder{
-        preventionForms = [];
-        cout << preventionForms
+        preventionForms = ["Don't smoke","Hydrate frequently by drinking enough water in the day","Do not hold urine if possible.","Consume enough fruits and veggies.","Avoid potential cancerous chemicals including: rubber, leather, printing materials, textiles, and paint substances."];
+        std::cout << preventionForms;
         symptoms = {{asymptomatic: "frequent urination","urination pain"},{symptomatic:"blood in urine","back pain","pain around pelvic area"}};
-        curingMethods = {asymptomatic: "drink less liquids",""};
+        curingMethods = {asymptomatic: "drink less liquids","Drink enough water each day and avoid caffeinated or alcoholic drinks, smoking, or spicy food."},{symptomatic:"Take a urine test to confirm this condition and take proper medication.","Take enough time to sit throughout the day and seek a physician for a proper diagnosis.","Wear comfortable clothing such as cotton underwear and elastic bottoms or pants."};
 
         for s in symptoms:
             if s == symptoms[0][0]:
-            cout << curingMethods[asymptomatic[0]]
+            cout << curingMethods[0][0]
             if s == symptoms[0][1]:
-            cout << curingMethods[asymptomatic[1]]
+            cout << curingMethods[0][1]
             if s == symptoms[1][0]:
-            cout << curingMethods[symptomatic[0]]
+            cout << curingMethods[1][0]
             if s == symptoms[1][1]:
-            cout << curingMethods[symptomatic[1]]
+            cout << curingMethods[1][1]
             if s == symptoms[1][2]:
-            cout << curingMethods[symptomatic[2]]
+            cout << curingMethods[1][2]
             else cout << "The patient does not have any of the asymptomatic or symptomatic symptoms. You do not have this form of cancer."
-
-
     };
     public static void Breast{
+        preventionForms =[];
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
 
     };
     public static void Cerival{};
