f7f281231f697bcc9bd1e21063263f04562a793a
chandram13 <mschandr@uci.edu>
2022-08-15 12:30:08 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int formsofCancer() {
+
+    std::cout << "In this program, the user will understand, diagnose, and prevent multiple forms of cancer." << std::endl;
+
+    public static void Bladder{};
+    public static void Breast{};
+    public static void Cerival{};
+    public static void Colorectal{};
+    public static void Gynecologic{};
+    public static void Headneck{};
+    public static void Kidney{};
+    public static void Liver{};
+    public static void Lung{};
+    public static void Lymphoma{};
+    public static void Mesothelioma{};
+    public static void Myeloma{};
+    public static void Ovarian{};
+    public static void Prostate{};
+    public static void Skin{};
+    public static void Thyroid{};
+    public static void Uterine{};
+    public static void vaginal&Vulvar{};
+    return 0;
+}
+
+
+
+
