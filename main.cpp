e18cdc966f2b67633ba5021cdcbdd09aaf4052a0
chandram13 <mschandr@uci.edu>
2022-08-15 13:32:19 -0700

    Updated bladder function with symptoms dictionary and began iteration of these conditions.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
-int formsofCancer() {
+class formsofCancer() {
 
     std::cout << "In this program, the user will understand, diagnose, and prevent multiple forms of cancer." << std::endl;
 
-    public static void Bladder{};
-    public static void Breast{};
+    public static void Bladder{
+        preventionForms = [];
+        cout << preventionForms
+        symptoms = [asymptomatic: "frequent urination","urination pain",symptomatic:"blood in urine","back pain","pain around pelvic area"];
+        curingMethods = [];
+
+        for s in symptoms:
+            if s == symptoms[0]:
+            cout << curingMethods[0]
+            if s == symptoms[1]:
+            cout << curingMethods[1]
+            if s == symptoms[2]:
+            cout << curingMethods[2]
+
+        if userExperiences == symptoms[0]:
+        cout << curingMethods[0]
+        if userExperiences == symptoms[1]:
+        cout << curingMethods[1]
+        if userExperiences == symptoms[2]:
+        cout << curingMethods[2]
+
+    };
+    public static void Breast{
+
+    };
     public static void Cerival{};
     public static void Colorectal{};
     public static void Gynecologic{};
