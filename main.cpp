53f368e38c373b5502402fb8bd83dcecbe15f18b
chandram13 <mschandr@uci.edu>
2022-08-15 20:06:25 -0700

    Added a try-catch exception for bladder patient in apparent case based on any symptoms of cancer is present.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ class formsofCancer() {
     std::cout << "In this program, the user will understand, diagnose, and prevent multiple forms of cancer." << std::endl;
 
     public static void Bladder{
-        preventionForms = ["Don't smoke","Hydrate frequently by drinking enough water in the day","Do not hold urine if possible.","Consume enough fruits and veggies.","Avoid potential cancerous chemicals including: rubber, leather, printing materials, textiles, and paint substances."];
+        preventionForms = ["Don't smoke.","Hydrate frequently by drinking enough water in the day.","Do not hold urine if possible.","Consume enough fruits and veggies.","Avoid potential cancerous chemicals including: rubber, leather, printing materials, textiles, and paint substances."];
         std::cout << preventionForms;
-        symptoms = {{asymptomatic: "frequent urination","urination pain"},{symptomatic:"blood in urine","back pain","pain around pelvic area"}};
-        curingMethods = {asymptomatic: "drink less liquids","Drink enough water each day and avoid caffeinated or alcoholic drinks, smoking, or spicy food."},{symptomatic:"Take a urine test to confirm this condition and take proper medication.","Take enough time to sit throughout the day and seek a physician for a proper diagnosis.","Wear comfortable clothing such as cotton underwear and elastic bottoms or pants."};
-
+        symptoms = {{Asymptomatic: "frequent urination","urination pain"},{Symptomatic:"blood in urine","back pain","pain around pelvic area"}};
+        curingMethods = {Asymptomatic: "drink less liquids","Drink enough water each day and avoid caffeinated or alcoholic drinks, smoking, or spicy food."},{Symptomatic:"Take a urine test to confirm this condition and take proper medication.","Take enough time to sit throughout the day and seek a physician for a proper diagnosis.","Wear comfortable clothing such as cotton underwear and elastic bottoms or pants."};
+        cancerStages = []
+        patientSick = cin >> haveSymptom
+        if haveSymptom == true;
+        try:
         for s in symptoms:
             if s == symptoms[0][0]:
             cout << curingMethods[0][0]
@@ -22,30 +25,126 @@ class formsofCancer() {
             if s == symptoms[1][2]:
             cout << curingMethods[1][2]
             else cout << "The patient does not have any of the asymptomatic or symptomatic symptoms. You do not have this form of cancer."
+
+            catch:
+            std::exception cout << "The patient has no apparent symptoms."
     };
     public static void Breast{
-        preventionForms =[];
+        preventionForms =["Maintain current weight","Exercise regularly","Don't drink or limit alcohol completely.","Be aware of your health if taking hormone replacement therapy medications.","Breastfeed your children, if you are a recently pregnant female.","If breast cancer is present in your family tree, speak with your physician."];
+        std::cout << preventionForms;
+        symptoms = {{Asymptomatic:"Irritation of breast skin","Pulling in of nipple or pain in nipple area","Change in size or breast shape","Any pain in breast areas"}{Symptomatic:"New lump near breast area or underarm.","Thickening or swelling near breast area","Redness or flaky skin near near nipples or breasts.","Any form of nipple discharge."}};
+        curingMethods ={Asymptomatic:"Seek a primary physician or consult a gynecologist.",Symptomatic:"Surgery to remove cancer tissue","Chemotherapy to shrink or kill cancer cells directly.","Hormonal therapy to block malignant tumors from developing.","Biological therapy to boost an individual's immune system.","Utilize radiation therapy specifically X-rays to eliminate cancer cells."};
+        for s in symptoms:
+            if s == symptoms[0][0]:
+            cout << curingMethods[0][0]
+            if s == symptoms[0][1]:
+            cout << curingMethods[0][0]
+            if s == symptoms[0][2]:
+            cout << curingMethods[0][0]
+            if s == symptoms[0][3]:
+            cout << curingMethods[0][0]
+            if s == symptoms[1][0]:
+            cout << curingMethods[1][0]
+
+        cancerStages = []
+
+    };
+    public static void Cerival{
+        preventionForms ={};
         std::cout << preventionForms;
         symptoms = {};
         curingMethods ={};
-
     };
-    public static void Cerival{};
-    public static void Colorectal{};
-    public static void Gynecologic{};
-    public static void Headneck{};
-    public static void Kidney{};
-    public static void Liver{};
-    public static void Lung{};
-    public static void Lymphoma{};
-    public static void Mesothelioma{};
-    public static void Myeloma{};
-    public static void Ovarian{};
-    public static void Prostate{};
-    public static void Skin{};
-    public static void Thyroid{};
-    public static void Uterine{};
-    public static void vaginal&Vulvar{};
+    public static void Colorectal{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Gynecologic{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Headneck{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Kidney{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Liver{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Lung{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Lymphoma{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Mesothelioma{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Myeloma{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Ovarian{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Prostate{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Skin{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Thyroid{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void Uterine{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
+    public static void vaginal&Vulvar{
+        preventionForms ={};
+        std::cout << preventionForms;
+        symptoms = {};
+        curingMethods ={};
+    };
     return 0;
 }
 
